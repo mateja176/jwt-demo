4cd65e9e9b9600028eddd8df52faa46856e70b87
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-22 23:22:17 +0200

    test(app): adds profile mock test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,34 +1,32 @@
-import { Express } from 'express';
+import * as express from 'express';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as request from 'supertest';
 import { Connection, createConnection } from 'typeorm';
 import { createApp } from './app';
 import mongoConfig from './config/mongo';
-
-let mongod: MongoMemoryServer;
-let connection: Connection;
-let app: Express;
-
-const initDb = async () => {
-  mongod = new MongoMemoryServer();
-  const uri = await mongod.getConnectionString();
-  connection = await createConnection({
-    ...mongoConfig,
-    url: uri,
-  });
-};
+import { User, UserDto } from './entity/User';
 
 describe('Server', () => {
-  beforeAll(() => {
-    // connection is undefined at this point
-    // if you need to connect to the database upon server initialization
-    // await initDb here
+  const mongod = new MongoMemoryServer();
+  let connection: Connection;
+  let app: express.Express;
+
+  beforeAll(async () => {
+    const uri = await mongod.getConnectionString();
+    connection = await createConnection({
+      ...mongoConfig,
+      url: uri,
+    });
     app = createApp(connection);
   });
-  beforeEach(async () => {
-    await initDb();
-  });
   afterEach(async () => {
+    await Promise.all(
+      connection.entityMetadatas
+        .map(({ tableName }) => tableName)
+        .map(tableName => connection.manager.clear(tableName)),
+    );
+  });
+  afterAll(async () => {
     await mongod.stop();
   });
   test('Hello World', done => {
@@ -36,6 +34,23 @@ describe('Server', () => {
       .get('/')
       .expect(200)
       .expect(JSON.stringify('Hello World'))
+      .end(done);
+  });
+  it('should return user profiles', async done => {
+    const testUser: UserDto = { email: 'test@test.com', password: '123' };
+    const user = User.create(testUser);
+    const userRepo = connection.getRepository(User);
+    await userRepo.save(user);
+
+    request(app)
+      .get('/profile')
+      .expect(200)
+      .expect(res =>
+        res.body.forEach(profile => {
+          delete profile.id;
+        }),
+      )
+      .expect([testUser])
       .end(done);
   });
 });
