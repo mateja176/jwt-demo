b1c346cd2ea152b95ce5c8d118fcc051c398de06
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-22 21:35:09 +0200

    feat(app.test): initializes app beforeAll

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,3 +1,4 @@
+import { Express } from 'express';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as request from 'supertest';
 import { Connection, createConnection } from 'typeorm';
@@ -6,21 +7,31 @@ import mongoConfig from './config/mongo';
 
 let mongod: MongoMemoryServer;
 let connection: Connection;
+let app: Express;
+
+const initDb = async () => {
+  mongod = new MongoMemoryServer();
+  const uri = await mongod.getConnectionString();
+  connection = await createConnection({
+    ...mongoConfig,
+    url: uri,
+  });
+};
 
 describe('Server', () => {
+  beforeAll(() => {
+    // connection is undefined at this point
+    // if you need to connect to the database upon server initialization
+    // await initDb here
+    app = createApp(connection);
+  });
   beforeEach(async () => {
-    mongod = new MongoMemoryServer();
-    const uri = await mongod.getConnectionString();
-    connection = await createConnection({
-      ...mongoConfig,
-      url: uri,
-    });
+    await initDb();
   });
   afterEach(async () => {
     await mongod.stop();
   });
   test('Hello World', done => {
-    const app = createApp(connection);
     request(app)
       .get('/')
       .expect(200)
