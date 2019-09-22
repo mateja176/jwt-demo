ba1d9d1032f99c529ecc92eb1c5b2071e3cbe91b
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-22 21:19:53 +0200

    refactor(app.test): mutates connection in beforeEach

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,28 +1,27 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as request from 'supertest';
-import { createConnection } from 'typeorm';
-import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
+import { Connection, createConnection } from 'typeorm';
 import { createApp } from './app';
 import mongoConfig from './config/mongo';
 
 let mongod: MongoMemoryServer;
-let config: MongoConnectionOptions;
+let connection: Connection;
 
 describe('Server', () => {
   beforeEach(async () => {
     mongod = new MongoMemoryServer();
     const uri = await mongod.getConnectionString();
-    config = {
+    connection = await createConnection({
       ...mongoConfig,
       url: uri,
-    };
+    });
   });
   afterEach(async () => {
     await mongod.stop();
   });
-  it('Hello World', async done => {
-    const connection = await createConnection(config);
-    request(createApp(connection))
+  test('Hello World', done => {
+    const app = createApp(connection);
+    request(app)
       .get('/')
       .expect(200)
       .expect(JSON.stringify('Hello World'))
