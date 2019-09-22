cf87c41e62a9be2b8587bf10201c9180a80b019a
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-22 14:59:12 +0200

    feat: removes unused constructEntity

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const constructEntity = <Dto, E extends Dto>(entity: E) => (
-  dto: Dto,
-) => {
-  Object.entries(dto).forEach(([key, value]) => {
-    entity[key] = value;
-  });
-
-  return entity;
-};
