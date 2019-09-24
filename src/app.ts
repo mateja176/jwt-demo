48f6d0329059008ae8d02226252b88440bde5a60
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-24 20:26:39 +0200

    feat(app): sets up password jwt authentication

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,73 @@
 import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
+import { isNil } from 'lodash';
+import * as passport from 'passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Connection } from 'typeorm';
+import { jwtSecret } from './config/jwt';
 import { User } from './entity/User';
 import { Request } from './models';
 import profileRoute from './routes/profile';
 
-export const createApp = (connection: Connection) => {
+// declare passport jwt based authentication middleware
+const authenticate = passport.authenticate('jwt', { session: false });
+// set up passport
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: jwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    ({ army }, done): void => {
+      done(null, army);
+    },
+  ),
+);
+
+export const createApp = (connection: Connection): express.Express => {
   const app = express();
 
   app.use((req: Request, res, next) => {
     req.db = connection;
     next();
   });
-
   app.use(express.json());
 
   app.get('/', (req, res) => {
     res.send('Hello World');
   });
-
   app.get('/users', async (req: Request, res) => {
     const users = await req.db.getRepository(User).find();
     res.json(users);
   });
 
-  app.use('/profile', profileRoute);
+  app.post(
+    '/join',
+    async (req: Request, res, next) => {
+      const { headers } = req;
+      if (!Object.keys(headers).includes('authorization')) {
+        // user has joined
+        const {
+          body: { name, armyCount },
+        } = req;
+        // TODO validate args
+        if (isNil(name) || isNil(armyCount)) {
+          res.status(400);
+          return next('name and army are required');
+        }
+        const token = jwt.sign({ army: { name, armyCount } }, jwtSecret);
+        return res.json({ token });
+      }
+      return next();
+    },
+    authenticate,
+    (req, res, next) => {
+      // user has returned
+      next('You have already joined');
+    },
+  );
+
+  app.use('/profile', authenticate, profileRoute);
 
   return app;
 };
