34de0c5e496ce801ab73ebb16d05ad7d594f7415
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-26 00:06:16 +0200

    feat(app): adds error and open connection handlers

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,17 @@ export const createApp = (connection: Connection): express.Express => {
     res.send('Hello World');
   });
 
+  app.get('/error', (req, res, next) => {
+    next('Hello World');
+  });
+
+  app.get('/open', (req, res) => {
+    res.write('Opened connection');
+    req.on('close', () => {
+      console.log('closed');
+    });
+  });
+
   app.get('/users', async (req: Request, res) => {
     const users = await req.db.getRepository(User).find();
 
